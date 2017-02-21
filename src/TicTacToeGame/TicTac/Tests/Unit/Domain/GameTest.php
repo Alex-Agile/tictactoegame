c3ed862450744bb5cfe8da794848802cc4d8220c
Alejandro Barba <alejandro@castoredc.com>
2017-02-21 17:20:19 +0100

    Step 05. Game test scaffolding

--- src/TicTacToeGame/TicTac/Tests/Unit/Domain/GameTest.php
+++ src/TicTacToeGame/TicTac/Tests/Unit/Domain/GameTest.php
@@ -3,10 +3,6 @@ declare(strict_types = 1);
 
 namespace TicTacToeGame\TicTac\Tests\Domain;
 
-use TicTacToeGame\TicTac\Domain\Game;
-use TicTacToeGame\TicTac\Domain\Grid\Board;
-use TicTacToeGame\TicTac\Domain\Grid\Cell;
-
 /**
  * Class GameTest
  *
@@ -21,55 +17,13 @@ class GameTest extends \PHPUnit_Framework_TestCase
      */
     public function testMovePositiveCase()
     {
-        // generate board mock
-        $boardMock = $this->getMockBuilder(Board::class)
-            ->disableOriginalConstructor()
-            ->setMethods([
-                    'init',
-                    'setGamePiece',
-                    'statusToArray',
-            ])
-            ->getMock();
-
-        // set board mock method expectations
-        $boardMock->expects($this->once())
-            ->method('init');
-
-        $boardMock->expects($this->once())
-            ->method('statusToArray')
-            ->willReturn([
-                ['','','',],
-                ['','','',],
-                ['','','',],
-            ]);
-
-        $boardMock->expects($this->once())
-            ->method('setGamePiece')
-            ->willReturn(true);
-
-        $game = new Game($boardMock);
-
-        $this->assertEquals($boardMock, $game->move(0, 0));
     }
 
     /**
-     * @expectedException \TicTacToeGame\TicTac\Exception\GameFinishStatusException
+     * expectedException \TicTacToeGame\TicTac\Exception\GameFinishStatusException
      */
     public function testMoveForAFinishGameShouldThrowAnException()
     {
-        // generate board mock
-        $boardMock = $this->getMockBuilder(Board::class)
-            ->disableOriginalConstructor()
-            ->setMethods([
-                    'init',
-                    'setGamePiece']
-            )
-            ->getMock();
-
-        $game = new Game($boardMock);
-        $game->setGameStatus(Game::GAME_STATUS_FINISH);
-
-        $this->assertEquals($boardMock, $game->move(0, 0));
     }
 
     /**
@@ -77,44 +31,5 @@ class GameTest extends \PHPUnit_Framework_TestCase
      */
     public function testMoveForAWinnerMoveShouldFinishTheGame()
     {
-        // generate board mock
-        $boardMock = $this->getMockBuilder(Board::class)
-            ->disableOriginalConstructor()
-            ->setMethods([
-                'init',
-                'setGamePiece',
-                'statusToArray',
-                'getBoardState',
-            ])
-            ->getMock();
-
-        // set board mock method expectations
-        $boardMock->expects($this->once())
-            ->method('init');
-
-        $boardMock->expects($this->once())
-            ->method('setGamePiece')
-            ->willReturn(true);
-
-        $boardMock->expects($this->once())
-            ->method('statusToArray')
-            ->willReturn([
-                ['X','O','',],
-                ['','X','O',],
-                ['','','X',],
-            ]);
-
-        $boardMock->expects($this->exactly(3))
-            ->method('getBoardState')
-            ->willReturn([
-                [new Cell(), new Cell(), new Cell()],
-                [new Cell(), new Cell(), new Cell()],
-                [new Cell(), new Cell(), new Cell()],
-            ]);
-
-        $game = new Game($boardMock);
-        $game->move(0, 0);
-
-        $this->assertEquals(Game::GAME_STATUS_FINISH, $game->getGameStatus());
     }
 }
